2d076e0c3b17a58069d0be22622a119e70f93b4e
Cristian Muscalu <muscaiu@yahoo.com>
2018-11-24 18:53:59 +0200

    added auto mode in version 2

---
+++ api/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyDu1V6eO6vQczNtecZdo8rVvFQAZB6llyg",
+  authDomain: "pompa-b855f.firebaseapp.com",
+  databaseURL: "https://pompa-b855f.firebaseio.com",
+  projectId: "pompa-b855f",
+  storageBucket: "pompa-b855f.appspot.com",
+  messagingSenderId: "145084180701"
+};
+
+module.exports = config;
--- api/index.js
+++ api/index.js
@@ -8,32 +8,28 @@ const fs = require('fs');
 const Gpio = require('onoff').Gpio;
 const relay = new Gpio(17, 'out');
 const axios = require('axios');
-const firebase = require("firebase");
 const { format } = require('date-fns');
+const pack = require('../package.json')
+
+const firebase = require("firebase");
+const config = require('./config');
+firebase.initializeApp(config);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const config = {
-  apiKey: "AIzaSyDu1V6eO6vQczNtecZdo8rVvFQAZB6llyg",
-  authDomain: "pompa-b855f.firebaseapp.com",
-  databaseURL: "https://pompa-b855f.firebaseio.com",
-  projectId: "pompa-b855f",
-  storageBucket: "pompa-b855f.appspot.com",
-  messagingSenderId: "145084180701"
-};
-firebase.initializeApp(config);
-
 const actionsRef = firebase.database().ref('actions');
 let lastAction = 'uninitialized';
+let mode = 'auto';
+let modeTime = 'uninitialized';
 let lastWeatherUpdate = 'uninitialized';
 
-const API_KEY = 'TBvCyGYowHIVOtFjgGwL1jAyw6dPpUix';
+// const API_KEY = 'TBvCyGYowHIVOtFjgGwL1jAyw6dPpUix';
 const ACU_API = 'http://dataservice.accuweather.com/currentconditions/v1';
 const CITY_KEY = '273756';
 
-function intervalFunc() {
+function watherCron() {
   axios.get(`${ACU_API}/${CITY_KEY}?apikey=TBvCyGYowHIVOtFjgGwL1jAyw6dPpUix`)
     .then(response => {
       lastWeatherUpdate = response.data[0]
@@ -42,19 +38,73 @@ function intervalFunc() {
       console.log(error);
     })
 }
-intervalFunc();
-setInterval(intervalFunc, 60 * 120000); //every 2 hours
+watherCron();
+setInterval(watherCron, 60 * 120000); //every 2 hours
+
 
 actionsRef.limitToLast(1).on('child_added', function (snap) {
   lastAction = snap.val().lastAction;
+  mode = snap.val().mode;
   lastStatus = snap.val().status;
+
   if (lastStatus) {
     relay.writeSync(0); //turn relay on
-  }else{
+  } else {
     relay.writeSync(1); //turn relay off
   }
   console.log('relay true status', relay.readSync() === 1 ? 'OFF' : 'ON');
 })
+
+function checkBetweenHours(first, second) {
+  const date = new Date();
+  // let current_minute = date.getMinutes();
+  let currentHour = date.gethours();
+  let currentValue = relay.readSync();
+  lastAction = Date.now();
+
+  console.log(currentHour, first, second, mode);
+  if (currentHour >= first && currentHour <= second) {
+    if (currentValue === 1) {
+      actionsRef.push({
+        lastAction,
+        status: currentValue,
+        mode,
+        humanDate: format(lastAction, 'HH:mm DD/MMM'),
+        lastWeatherUpdate
+      });
+
+      relay.writeSync(0); //turn relay on
+      console.log('relay was off, turning it on')
+    } else {
+      console.log('relay is on already')
+    }
+  } else {
+    if (currentValue === 0) {
+      actionsRef.push({
+        lastAction,
+        status: currentValue,
+        mode,
+        humanDate: format(lastAction, 'HH:mm DD/MMM'),
+        lastWeatherUpdate
+      });
+
+      relay.writeSync(1); //turn relay off
+      console.log('relay was on, turning it off')
+    } else {
+      console.log('relay is off already')
+    }
+  }
+}
+
+function timeCron() {
+  if (mode === 'auto') {
+    checkBetweenHours(19, 20)
+  }
+}
+// timeCron();
+setInterval(timeCron, 1800000); //every hour
+// setInterval(timeCron, 10000); //every 10 sec
+
 
 router.get('/light', function (req, res) {
   let currentValue = relay.readSync();
@@ -62,20 +112,21 @@ router.get('/light', function (req, res) {
     {
       status: currentValue,
       lastAction,
-      lastWeatherUpdate
+      mode,
+      lastWeatherUpdate,
+      apiVersion: pack.version
     }
   );
 });
 
 router.post('/light', function (req, res) {
   console.log('change status to', req.body.status)
-
   lastAction = Date.now();
 
   actionsRef.push({
     lastAction,
     status: req.body.status,
-    type: 'manual',
+    mode,
     humanDate: format(lastAction, 'HH:mm DD/MMM'),
     lastWeatherUpdate
   });
@@ -98,6 +149,18 @@ router.post('/login', function (req, res) {
     lastAction = Date.now();
     console.log(req.body, 'access granted')
     res.json({ access: true, lastAction })
+  } else {
+    console.log(req.body, 'access denied')
+    res.json({ access: false })
+  }
+});
+
+router.post('/mode', function (req, res) {
+  if (req.body.password === 'orhideelor') {
+    mode = req.body.mode;
+    modeTime = Date.now();
+    console.log(mode, 'access granted')
+    res.json({ access: true, mode, modeTime })
   } else {
     console.log(req.body, 'access denied')
     res.json({ access: false })
